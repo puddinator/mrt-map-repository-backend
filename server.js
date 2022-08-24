99c97ffb2298b5d968751087c607965484e6109a
puddinator <jamesgaowenzhen@gmail.com>
2022-08-25 00:26:31 +0800

    change cors header

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use(
 );
 app.use(compression());
 app.use(express.json());
+app.use((res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use("/", routes);
 app.use("/uploads", express.static("./uploads"));
 
